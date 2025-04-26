1486b8620aa15edd883d419bd8540c39522cc764
okmethod <okmethod2020@gmail.com>
2025-04-26 14:30:37 +0900

    :shirt: uv add mypy --dev

--- vox-server/src/sample.py
+++ vox-server/src/sample.py
@@ -16,18 +16,18 @@ def main() -> None:
 
     # Synthesizerの初期化
     voicevox_onnxruntime_path = (
-        voicevox_core_path / "onnxruntime" / "lib" / Onnxruntime.LIB_VERSIONED_FILENAME
+        voicevox_core_path / "onnxruntime" / "lib" / Onnxruntime.LIB_VERSIONED_FILENAME   # type: ignore[attr-defined]
     )
     open_jtalk_dict_dir = voicevox_core_path / "dict" / "open_jtalk_dic_utf_8-1.11"
     synthesizer = Synthesizer(
-        Onnxruntime.load_once(filename=str(voicevox_onnxruntime_path)),
+        Onnxruntime.load_once(filename=str(voicevox_onnxruntime_path)),   # type: ignore[attr-defined]
         OpenJtalk(open_jtalk_dict_dir),
     )
 
     # 音声モデルの読み込み
     voicevox_model_name = "0.vvm"
     voice_model_path = voicevox_core_path / "models" / "vvms" / voicevox_model_name
-    with VoiceModelFile.open(voice_model_path) as model:
+    with VoiceModelFile.open(voice_model_path) as model:   # type: ignore[attr-defined]
         synthesizer.load_voice_model(model)
 
     # テキスト音声合成
